80b03f15804e18ff3743d0e0a8c17e0bf05bd61f
Matthew Gamble <git@matthewgamble.net>
2022-06-07 01:13:38 +1000

    Override (fix) color depth detection

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,28 @@
-import { Cli, Builtins } from "clipanion";
+import process from "node:process";
+import type { stdout } from "node:process";
+
+import { Cli as BaseCli, Builtins } from "clipanion";
 
 import { LintCommand } from "./commands/lint.js";
+
+function getColorDepth() {
+  if ("getColorDepth" in process.stdout) {
+    return process.stdout.getColorDepth();
+  }
+
+  if ("isTTY" in process.stdout && (process.stdout as typeof stdout).isTTY) {
+    return 8;
+  }
+
+  return 1;
+}
+
+class Cli extends BaseCli {
+  // We need to override this because the built-in detection is broken.
+  override format() {
+    return super.format(getColorDepth() > 1);
+  }
+}
 
 export function makeCli(): Cli {
   const cli = new Cli({
