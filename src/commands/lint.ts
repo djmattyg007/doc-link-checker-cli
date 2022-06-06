ac012cb111e563194c2b79fc20595d274cf86311
Matthew Gamble <git@matthewgamble.net>
2022-06-06 22:16:44 +1000

    Initial code for main lint command

---
+++ src/commands/lint.ts
@@ -0,0 +1,91 @@
+import { Command, Option } from "clipanion";
+import * as t from "typanion";
+
+import { markdownOptions, scanFiles, verifyLinks } from "doc-link-checker";
+
+import { fileErrorMsgs, anchorErrorMsgs } from "../error-msg.js";
+
+const DEFAULT_INCLUDE_GLOBS: ReadonlyArray<string> = [
+  "**/*.md",
+  "**/*.mdown",
+  "**/*.markdown",
+];
+const DEFAULT_EXCLUDE_GLOBS: ReadonlyArray<string> = [
+  "**/node_modules/**",
+  "**/.venv/**",
+  "**/venv/**",
+  "**/vendor/**",
+];
+
+const exitCode = t.cascade(t.isNumber(), [
+  t.isInteger(),
+  t.isInInclusiveRange(1, 255),
+]);
+
+export class LintCommand extends Command {
+  static override paths = [["lint"]];
+
+  caseSensitive = Option.Boolean("--case-sensitive", false);
+  mdType = Option.String("--md-type", markdownOptions.mdDefaultType, { validator: t.isEnum(markdownOptions.mdTypes) });
+
+  include = Option.Array("--include", []);
+  exclude = Option.Array("--exclude", []);
+
+  includeExtend = Option.Array("--include-extend", []);
+  excludeExtend = Option.Array("--exclude-extend", []);
+
+  successCode = Option.String("--success-code", "0", { validator: exitCode });
+  failureCode = Option.String("--failure-code", "1", { validator: exitCode });
+
+  static override schema = [
+    t.hasMutuallyExclusiveKeys(["include", "includeExtend"]),
+    t.hasMutuallyExclusiveKeys(["exclude", "excludeExtend"]),
+  ];
+
+  async execute(): Promise<number> {
+    const includeGlobs = this.include.length > 0
+      ? this.include
+      : DEFAULT_INCLUDE_GLOBS.concat(this.includeExtend);
+    const excludeGlobs = this.exclude.length > 0
+      ? this.exclude
+      : DEFAULT_EXCLUDE_GLOBS.concat(this.excludeExtend);
+
+    const scanOptions = {
+      basePath: process.cwd(),
+      caseSensitive: this.caseSensitive,
+      mdType: this.mdType,
+    };
+
+    let foundAnyError = false;
+    const scan = scanFiles(includeGlobs, excludeGlobs, scanOptions);
+    for await (const result of scan) {
+      let foundError = false;
+      const verify = verifyLinks(scanOptions.basePath, result.file, result.links);
+      for await (const verifyError of verify) {
+        if (foundError === false) {
+          this.context.stdout.write(`--- ${result.file.path} ---`);
+          foundError = true;
+        }
+
+        const errorMsg = verifyError.errorType === "file" ? fileErrorMsgs[verifyError.errorCode] : anchorErrorMsgs[verifyError.errorCode];
+
+        const position = verifyError.link.position;
+        const lineMarker = position ? String(position.start.line) : "?";
+        const href = verifyError.link.href;
+        this.context.stdout.write(`line ${lineMarker}: ${href} (${errorMsg})`);
+      }
+
+      if (foundError === true) {
+        foundAnyError = true;
+      } else {
+        this.context.stdout.write(`${result.file.path} [OK]`);
+      }
+    }
+
+    if (foundAnyError === true) {
+      return this.failureCode;
+    } else {
+      return this.successCode;
+    }
+  }
+}
---
+++ src/error-msg.ts
@@ -0,0 +1,21 @@
+import { FileCheckResponse, AnchorCheckResponse } from "doc-link-checker";
+
+export const fileErrorMsgs: { [errorCode in FileCheckResponse]: string } = {
+  [FileCheckResponse.SUCCESS]: "No error.",
+  [FileCheckResponse.FILE_NOT_EXISTS]: "Link references a file that does not exist.",
+  [FileCheckResponse.FILE_OUTSIDE_BASE]: "Link references a file that exists outside of the base directory.",
+  [FileCheckResponse.CONVERT_PURE_ANCHOR]: "Link references the file it is in.",
+};
+
+export const anchorErrorMsgs: { [errorCode in AnchorCheckResponse]: string } = {
+  [AnchorCheckResponse.EMPTY_ANCHOR]: "Link includes an empty anchor.",
+  [AnchorCheckResponse.BINARY_FILE]: "Link targets a binary file, which means there is no useful way to target individual sections of the file with an anchor.",
+  [AnchorCheckResponse.ANCHOR_UNDISCOVERABLE]: "Link targets a file with no file extension, so it cannot be determined if the anchor is valid.",
+  [AnchorCheckResponse.NO_ANCHORS_IN_FILETYPE]: "Link targets a non-document file with an anchor that isn't supported for non-document files.",
+  [AnchorCheckResponse.HEADING_MATCH_SUCCESS]: "No error.",
+  [AnchorCheckResponse.HEADING_MATCH_FAIL]: "Link references a non-existent header.",
+  [AnchorCheckResponse.LINE_TARGET_SUCCESS]: "No error.",
+  [AnchorCheckResponse.LINE_TARGET_FAIL]: "Link references a non-existent line number.",
+  [AnchorCheckResponse.LINE_TARGET_INVALID]: "Link anchor contains invalid line number reference.",
+  [AnchorCheckResponse.MULTI_LINE_TARGET_RANGE_INVALID]: "The start number of a multi-line anchor must be less than the end number.",
+};
