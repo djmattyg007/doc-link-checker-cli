1e2705dfe99c0251db3f2582b5d434eaa7750d3a
Matthew Gamble <git@matthewgamble.net>
2022-06-07 00:19:37 +1000

    Use proper quotes in CLI constructor

--- src/cli.ts
+++ src/cli.ts
@@ -4,9 +4,9 @@ import { LintCommand } from "./commands/lint.js";
 
 export function makeCli(): Cli {
   const cli = new Cli({
-    binaryLabel: `Doc Link Checker`,
-    binaryName: `doc-link-chcker`,
-    binaryVersion: `1.0.0`,
+    binaryLabel: "Doc Link Checker",
+    binaryName: "doc-link-chcker",
+    binaryVersion: "1.0.0",
   });
 
   cli.register(LintCommand);
