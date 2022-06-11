644a7b8755afc3c74f219f9b0a7bc898f288fc14
Matthew Gamble <git@matthewgamble.net>
2022-06-11 12:35:05 +1000

    Bump version - v1.0.1

--- src/cli.ts
+++ src/cli.ts
@@ -28,7 +28,7 @@ export function makeCli(): Cli {
   const cli = new Cli({
     binaryLabel: "Doc Link Checker",
     binaryName: "doc-link-chcker",
-    binaryVersion: "1.0.0",
+    binaryVersion: "1.0.1",
   });
 
   cli.register(LintCommand);
